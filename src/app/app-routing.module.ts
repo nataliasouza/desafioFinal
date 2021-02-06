ce11d005be6d193601bb68455be292ccf612a5fe
nataliasouza <nataliasouza.ti@hotmail.com>
2021-02-06 17:50:02 -0300

    Base do Projeto Final

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PainelUsuarioComponent } from './painel-usuario/painel-usuario.component';
+
+
+
+const routes: Routes = [ {
+  path: '',
+  component: HomeComponent
+},{
+  path: 'home',
+  component: HomeComponent,
+},{
+  path: 'login',
+  component: LoginComponent,
+},{
+  path: 'painel-usuario',
+  component: PainelUsuarioComponent,
+},{
+  path: 'cadastro',
+  component: CadastroComponent,
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PainelUsuarioComponent } from './painel-usuario/painel-usuario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    CadastroComponent,
+    PainelUsuarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
