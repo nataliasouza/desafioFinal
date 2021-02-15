03a774e098b156b07014835df9d0e1f922c5c134
nataliasouza <nataliasouza.ti@hotmail.com>
2021-02-15 03:40:35 -0300

    Criei o componente para criar as postagens

---
+++ src/app/profile-post/profile-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-post',
+  templateUrl: './profile-post.component.html',
+  styleUrls: ['./profile-post.component.scss']
+})
+export class ProfilePostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
