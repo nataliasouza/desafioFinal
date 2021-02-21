701406dfc9f4a2aefb37d77f29f6348f42b83fd3
AmandaJPicoli <amandajardimpicoli@gmail.com>
2021-02-21 14:25:51 -0300

    Consumo da api + postagem + login

---
+++ src/app/services/postagens/postagem.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Postage } from '../model/Postage.Interface';
+import { ProviderService } from '../providers/provider.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService extends ProviderService{
+
+  constructor(private http: HttpClient ) 
+  { 
+    super("Postage");
+
+  }
+
+
+ // POSTAGENS
+
+  getPostagesById(id: string) {
+    let postagem = this.http.get<any>(`${this.url}`);
+    return postagem;
+  }
+
+  postPostage(postagem: Postage) {
+    return this.http.post<any>(`${this.url}`, postagem);
+  }
+
+
+ 
+}
