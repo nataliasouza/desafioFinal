c7f68141095a0dc6b1e90351a058623e1078614d
AmandaJPicoli <amandajardimpicoli@gmail.com>
2021-02-22 01:35:51 -0300

    Cadastro atualizado

--- src/app/services/postagens/postagem.service.ts
+++ src/app/services/postagens/postagem.service.ts
@@ -6,14 +6,13 @@ import { ProviderService } from '../providers/provider.service';
 @Injectable({
   providedIn: 'root'
 })
-export class PostagemService extends ProviderService{
+export class PostagemService extends ProviderService {
 
-  constructor(private http: HttpClient ) 
-  { 
+  constructor(private http: HttpClient) {
     super("Postage");
   }
-  
- // POSTAGENS
+
+  // POSTAGENS
 
   getPostagesById() {
     return this.http.get<any>(`${this.url}`);
@@ -25,10 +24,16 @@ export class PostagemService extends ProviderService{
 
   // LIKES
 
-postLike(id){
-  return this.http.post<any>(`${this.url}/Likes`, id);
-}
+  postLike(id) {
+    return this.http.post<any>(`${this.url}/Likes`, id);
+  }
 
+  postComments(id: number, comment: any) {
+    return this.http.post<any>(`${this.url}/${id}/Comments`, comment);
+  }
 
- 
+  getComments(id: number){
+    return this.http.get<any>(`${this.url}/${id}/Comments`);
+  }
+
 }
